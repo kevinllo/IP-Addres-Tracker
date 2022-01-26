87c84e26acb8e5be0b8b38af67035d16f2ec7d5f
Kevin Grande <kevingrande2002@gmail.com>
2022-01-26 13:39:54 -0600

    ADD: header

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Container>
+      <Title>IP Adress Tracker</Title>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 250px;
+  padding: 20px 0;
+  outline: 1px solid black;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: url("/images/pattern-bg.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    z-index: -1;
+  }
+`;
+const Title = styled.h1`
+  text-align: center;
+  color: white;
+  font-weight: 300;
+`;
